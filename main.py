e8ede7863f8f3ea58db2fdd4dec9fa7f4bd258b9
Aliyah Shahid <aliyahshahid@me.com>
2021-11-24 23:09:47 +0000

    Pseudocode on led stuff

--- main.py
+++ main.py
@@ -1,66 +1,109 @@
 # Imports
 
 import cv2
-import numpy as np
+import numpy as np#
 
-# Create a VideoCapture object and read from input file
+# Imports for the led control
 
-capture = cv2.VideoCapture(0)
+# RPi.GPIO allows us to control all GPIOs from the raspberry pi GPIO header
 
-# Checking if camera is opened successfully
+import RPi.GPIO as GPIO
 
-if (capture.isOpened() == False):
-    print("Error opening the video file")
+# Time module allows us to turn the leds on for certain amounts of times
 
-# Read until live video is over
+import time
 
-while (capture.isOpened()):
+# LED_PIN = 17
 
-    # Capture frame-by-frame
+# This creates a constant global variable with the GPIO number for the LED
 
-    ret, frame = capture.read()
+# GPIO.setmode(GPIO.BCM)
 
-    if ret == True:
+# https://roboticsbackend.com/raspberry-pi-control-led-python-3/
 
-    # Display the resulting frame
+# # The 1 value is there because I want it to load and read a colour image
+# CFB_img = cv2.imread('CaramelColourStages/CFB.jpg', 1)
+# CBS_img = cv2.imread('CaramelColourStages/CBS.jpg', 1)
+#
+# CFB_hsv_img = cv2.cvtColor(CFB_img, cv2.COLOR_BGR2HSV)
+# CBS_hsv_img = cv2.cvtColor(CBS_img, cv2.COLOR_BGR2HSV)
+#
+# print(CFB_hsv_img)
+# print(CBS_hsv_img)
+#
+# # Create a VideoCapture object and read from input file
+#
+# capture = cv2.VideoCapture(0)
+#
+# # Checking if camera is opened successfully
+#
+# if (capture.isOpened() == False):
+#     print("Error opening the video file")
+#
+# # Read until live video is over
+#
+# while (capture.isOpened()):
+#
+#     # Capture frame-by-frame
+#
+#     ret, frame = capture.read()
+#
+#     if ret == True:
+#
+#     # Display the resulting frame
+#
+#         # cv2.imshow('Frame', frame)
+#
+#     # Press Q on keyboard to quit
+#
+#         if cv2.waitKey(25) & 0xFF == ord('q'):
+#             break
+#
+#         # Convert the colour space from BGR to HSV
+#
+#         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+#
+#         # Pick a lower and upper bound for pixels we want to extact
+#
+#         # Check the hsv colour wheel to put inside the square brackets
+#
+#         lower_caramel = np.array([CFB_hsv_img])
+#         upper_caramel = np.array([CBS_hsv_img])
+#
+#         # Mask = portion of an image - tells you which parts of the image you should keep
+#         # The mask blocks out all the other colours other than the ones in the range of what youre wanting
+#
+#         mask = cv2.inRange(hsv, lower_blue, upper_blue)
+#
+#         # This line is what picks out what colours match and what doesn't and turns it to black
+#
+#         result = cv2.bitwise_and(frame, frame, mask=mask)
+#
+#         cv2.imshow('frame', result)
+#
+#     # Break the loop
+#     else:
+#         break
+#
+# # Release the video capture object when everything is done
+#
+# capture.release()
+#
+# # Closes all the frames
+#
+# cv2.destroyAllWindows()
 
-        cv2.imshow('Frame', frame)
+color = range of colours in acceptable form
 
-    # Press Q on keyboard to quit
+if cameracolour is between lowercolour and lowestofcolour then
+    yellow light
 
-        if cv2.waitKey(25) & 0xFF == ord('q'):
-            break
+    else if cameracolour = colour
+    greenlight
 
-        # Convert the colour space from BGR to HSV
+    else if cameracolour is between highest colour and burnt colour max then
+    redlight
 
-        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    else
+    "Please point the camera at the caramel"
 
-        # Pick a lower and upper bound for pixels we want to extact
-
-        # Check the hsv colour wheel to put inside the square brackets
-
-        lower_blue = np.array([])
-        upper_blue = np.array([])
-
-        # Mask = portion of an image - tells you which parts of the image you should keep
-        # The mask blocks out all the other colours other than the ones in the range of what youre wanting
-
-        mask = cv2.inRange(hsv, lower_blue, upper_blue)
-
-        # This line is what picks out what colours match and what doesn't and turns it to black
-
-        result = cv2.bitwise_and(frame, frame, mask=mask)
-
-        cv2.imshow('frame', result)
-
-    # Break the loop
-    else:
-        break
-
-# Release the video capture object when everything is done
-
-capture.release()
-
-# Closes all the frames
-
-cv2.destroyAllWindows()
