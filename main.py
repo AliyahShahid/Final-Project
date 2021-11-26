3927f0d38f236cbf0a6e6cc14671f11d5ecf52fb
AliyahShahid <44412909+AliyahShahid@users.noreply.github.com>
2021-11-26 15:29:10 +0000

    ..

--- main.py
+++ main.py
@@ -6,99 +6,90 @@ import matplotlib.pyplot as plt
 
 # Loading the sugar image to detect it initially so that I can then track the stage of the cooking process
 
-sugar_img = cv2.imread('CaramelColourStages/Sugar.jpg', cv2.IMREAD_GRAYSCALE)
-uneditedSugar_img = cv2.imread('CaramelColourStages/UneditedSugar.jpg', cv2.IMREAD_GRAYSCALE)
-
-# Display the image (so that I can see when testing the idea)
-
-cv2.imshow("Sugar", sugar_img)
-cv2.imshow("Unedited Sugar", uneditedSugar_img)
-
-# Initiating ORB detector
-
-orb = cv2.ORB_create()
-
-# Finding the keypoints and descriptors with ORB
-
-kp1, des1 = orb.detectAndCompute(sugar_img, None)
-kp2, des2 = orb.detectAndCompute(uneditedSugar_img, None)
-
-# Creating the BFMatcher object
-
-
-bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-
-# Matching the descriptors
-
-matches = bf.match(des1, des2)
-
-# Sorting them in the order of their distance
-
-matches = sorted(matches, key = lambda x:x.distance)
-
-# Drawing the first 10 matches
-
-img3 = cv2.drawMatches(sugar_img, kp1, uneditedSugar_img, kp2, matches[:10], None, flags = cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
-
-plt.imshow(img3), plt.show()
-
-cv2.waitKey(0)
-
-# Converting the image to a grayscale so that less information needs to be provided for each pixel. I think it's already in greyscale but just to be sure
-
-# sugar_img_gray = cv2.cv2tColor(sugar_img, cv2.COLOR_BGR2GRAY)
-
-# Showing the grayscale image
-
-# cv2.imshow("Gray image", sugar_img_gray)
+# sugar_img = cv2.imread('CaramelColourStages/Sugar.jpg', cv2.IMREAD_GRAYSCALE)
+# uneditedSugar_img = cv2.imread('CaramelColourStages/UneditedSugar.jpg', cv2.IMREAD_GRAYSCALE)
 #
+# # Display the image (so that I can see when testing the idea)
+#
+# cv2.imshow("Sugar", sugar_img)
+# cv2.imshow("Unedited Sugar", uneditedSugar_img)
+#
+# # Initiating ORB detector
+#
+# orb = cv2.ORB_create()
+#
+# # Finding the keypoints and descriptors with ORB
+#
+# kp1, des1 = orb.detectAndCompute(sugar_img, None)
+# kp2, des2 = orb.detectAndCompute(uneditedSugar_img, None)
+#
+# # Creating the BFMatcher object
+#
+# bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+#
+# # Matching the descriptors
+#
+# matches = bf.match(des1, des2)
+#
+# # Sorting them in the order of their distance
+#
+# matches = sorted(matches, key = lambda x:x.distance)
+#
+# # Drawing the first 10 matches
+#
+# img3 = cv2.drawMatches(sugar_img, kp1, uneditedSugar_img, kp2, matches[:10], None, flags = cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+#
+# plt.imshow(img3), plt.show()
+
 # cv2.waitKey(0)
+
+# NOT HERE Converting the image to a grayscale so that less information needs to be provided for each pixel. I think it's already in greyscale but just to be sure
 
 # Create a VideoCapture object and read from input file
 
-# capture = cv2.VideoCapture(0)
-#
-# # Checking if camera is opened successfully
-#
-# if (capture.isOpened() == False):
-#     print("Error opening the video file")
-#
-# # Read until live video is over
-#
-# while (capture.isOpened()):
-#
-#     # Capture frame-by-frame
-#
-#     ret, frame = capture.read()
-#
-#     # Sugar image for detecting the beginning stage
-#
-#     sugar_vid = 'CaramelColourStages/Sugar.jpg'
-#
-#     sugar_vid_gray = cv2.cv2tColor(sugar_vid, cv2.COLOR_BGR2GRAY)
-#
-#     if ret == True:
-#
-#     # Display the resulting frame
-#
-#         cv2.imshow('Frame', frame)
-#
-#     # Press Q on keyboard to quit
-#
-#         if cv2.waitKey(25) & 0xFF == ord('q'):
-#             break
-#
-#     # Break the loop
-#     else:
-#         break
-#
-# # Release the video capture object when everything is done
-#
-# capture.release()
-#
-# # Closes all the frames
-#
-# cv2.destroyAllWindows()
+capture = cv2.VideoCapture('Sugar_vid.MP4')
+
+# Checking if camera is opened successfully
+
+if (capture.isOpened() == False):
+    print("Error opening the video stream")
+
+# Read until live video is over
+
+while (capture.isOpened()):
+
+    # Capture frame-by-frame
+
+    ret, frame = capture.read()
+
+    # Sugar image for detecting the beginning stage
+
+    # sugar_vid = 'CaramelColourStages/Sugar.jpg'
+    #
+    # sugar_vid_gray = cv2.cv2tColor(sugar_vid, cv2.COLOR_BGR2GRAY)
+
+    if ret == True:
+
+    # Display the resulting frame
+
+        cv2.imshow('Frame', frame)
+
+    # Press Q on keyboard to quit
+
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            break
+
+    # Break the loop
+    else:
+        break
+
+# Release the video capture object when everything is done
+
+capture.release()
+
+# Closes all the frames
+
+cv2.destroyAllWindows()
 
 # # Convert the colour space from BGR to HSV
 #
@@ -142,11 +133,11 @@ cv2.waitKey(0)
 
 # RPi.GPIO allows us to control all GPIOs from the raspberry pi GPIO header
 
-import RPi.GPIO as GPIO
+# import RPi.GPIO as GPIO
 
 # Time module allows us to turn the leds on for certain amounts of times
 
-import time
+# import time
 
 # LED_PIN = 17
 
